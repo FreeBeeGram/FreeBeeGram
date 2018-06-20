e9b18ae461a4b859d4c0026f6104053c1c412dd0
sleex89 <samlee723@gmail.com>
2018-06-20 11:33:39 -0700

    Added Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
   }
   render() {
     return(
-      <div className={styles.HeaderContainer}>
+      <div className={styles.Header}>
         <div className={styles.Logo}>
           FreeBeeGram
         </div>
