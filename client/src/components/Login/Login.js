24cae6bc63e46b78a4089f13c21ea0be9c25303b
sleex89 <samlee723@gmail.com>
2018-06-24 23:20:10 -0700

    Finished layout of Login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -10,9 +10,27 @@ class Login extends Component {
         </div>
         <div className={styles.BodyContainer}>
           <div className={styles.BodyMessage}>Welcome Back</div>
-          <div>
-            <div></div>
-            <div></div>
+          <div className={styles.FormsContainer}>
+            <div className={styles.LoginForms}>
+              <div className={styles.FormHeader}>I'm a Creator</div>
+              <form className={styles.LoginForm}>
+                <input placeholder="E-mail"/>
+                <input placeholder="Password"/>
+                <div>Forgot?</div>
+                <button className={styles.Button}>SIGN IN</button>
+                <div className={styles.Footer}>Need an account? I'm a Creator</div>
+              </form>
+            </div>
+            <div className={styles.LoginForms}>
+              <div className={styles.FormHeader}>I'm a Brand</div>
+                <form className={styles.LoginForm}>
+                  <input placeholder="E-mail"/>
+                  <input placeholder="Password"/>
+                  <div>Forgot?</div>
+                  <button className={styles.Button}>SIGN IN</button>
+                  <div className={styles.Footer}>Need an account? I'm a Brand</div>
+                </form>
+            </div>
           </div>
         </div>
       </div>
