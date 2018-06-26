ea2634e41326361fada9d5f4e0b5ce3136db7580
sleex89 <samlee723@gmail.com>
2018-06-25 21:11:31 -0700

    Created generic filters section

--- client/src/components/Creator/CreatorFilter/CreatorFilter.js
+++ client/src/components/Creator/CreatorFilter/CreatorFilter.js
@@ -3,8 +3,23 @@ import styles from './CreatorFilter.css';
 
 const creatorFilter = (props) => {
   return (
-    <div>
-      
+    <div className={styles.CreatorFilter}>
+      <div>
+        <div className={styles.CategoryTitle}>Category</div>
+        <div>Electronics</div>
+        <div>Fashion</div>
+        <div>Beauty</div>
+        <div>Home</div>
+        <div>Pet</div>
+        <div>Other</div>
+      </div>
+      <div>
+        <div className={styles.CategoryBrands}>Brands</div>
+        <div>DadHatStudio</div>
+        <div>Nike</div>
+        <div>Adidas</div>
+        <div>Supreme</div>
+      </div>
     </div>
   )
 }
