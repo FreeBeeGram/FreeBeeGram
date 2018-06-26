1d4ba6faf6aaa87ed441d0487f94f98fc596f457
sleex89 <samlee723@gmail.com>
2018-06-25 20:57:33 -0700

    Created creatorFilter component

--- client/src/components/Creator/Creator.js
+++ client/src/components/Creator/Creator.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import styles from './Creator.css';
 import CreatorHeader from './CreatorHeader/CreatorHeader';
+import CreatorFilter from './CreatorFilter/CreatorFilter';
 
 class Creator extends Component {
   render() {
     return (
-      <CreatorHeader />
+      <div>
+        <CreatorHeader />
+        <div className={styles.CreatorMain}>
+          <CreatorFilter />
+          <div>world</div>
+        </div>
+      </div>
     )
   }
 }
---
+++ client/src/components/Creator/CreatorFilter/CreatorFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CreatorFilter.css';
+
+const creatorFilter = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default creatorFilter;
