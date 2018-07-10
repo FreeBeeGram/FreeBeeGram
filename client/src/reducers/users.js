8afd421bfb5233d0c311ac43106c0bdc14861bd5
sleex89 <samlee723@gmail.com>
2018-07-10 15:01:31 -0700

    users reducer

--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -1,5 +1,9 @@
+import { USER_SIGNUP } from 'actions/types'
+
 export default function(state = [], action) {
   switch (action.type) {
+    case USER_SIGNUP:
+      return [...state, action.payload];
     default:
       return state;
   }
