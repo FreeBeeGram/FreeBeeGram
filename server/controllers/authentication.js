9132044b90ea4a4693994e59299d497ee154954a
sleex89 <samlee723@gmail.com>
2018-07-10 17:33:52 -0700

    Encrypted password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,6 +4,10 @@ exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide an email and a password' })
+  }
+
   // See if a user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) { return next(err); }
@@ -11,8 +15,6 @@ exports.signup = function(req, res, next) {
     if (existingUser) {
       return res.status(422).send({ error: 'Email is in use' });
     }
-
-  
 
   // If a user with email does exist, return an error
     const user = new User({
@@ -22,9 +24,9 @@ exports.signup = function(req, res, next) {
 
     user.save(function(err) {
       if (err) { return next(err); }
-      
+
       // If a user with email does NOT exist, create and save user record
-      res.json(user);
+      res.json({ success: true });
     })
   });
   
